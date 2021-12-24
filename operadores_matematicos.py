b22a295b3268cc795a10df56808356818470d43c
gabriel <gabriel.diaz@avalora.com>
2021-12-23 22:06:35 -0300

    operadores matematicos

---
+++ operadores_matematicos.py
@@ -0,0 +1,30 @@
+# Operadores Matematicos
+5 + 5  # 10
+5 * 3  # 15
+5 / 5  # 1.0
+5 // 5  # 1 (retorna sin decimal)
+10 % 4  # 2 (retorna el resto de la division)
+33 ** 3 # 27 (retorna el exponente)
+
+type(1)  # <class 'int'>
+type('hola')  # <class 'str'>
+
+'a' + 'b'  # ab
+'b' * 5  # bbbbb
+
+# Se los asigna a la misma variable
+a = 5
+a += 2
+print(a)  # 7
+b = 2
+b *= 5
+print(b)  # 10
+c = 3
+c /= 3
+print(c)  # 1.0
+d = 4
+d //= 2
+print(d) # 2
+e = 6
+e -= 3
+print(e) # 3
